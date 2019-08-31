8fa5543b50e5c7c0156c60be7dfad6b528eb141c
Thomas Jeng <tjeng831@berkeley.edu>
2019-08-30 18:39:04 -0700

    adding

--- Desktop/spotify/client/src/App.js
+++ Desktop/spotify/client/src/App.js
@@ -7,85 +7,100 @@ const spotifyWebApi = new Spotify();
 
 class App extends Component {
  constructor(){
-   super();
-   const params = this.getHashParams();
-   const token = params.access_token;
-   if (token) {
-     spotifyWebApi.setAccessToken(token);
-   }
-   this.state = {
-     userPlaylists : '',
-     namesList: '',
-     playlistOne : '',
-     playlistTwo : ''
-   }
- }
-  getHashParams() {
-   var hashParams = {};
-   var e, r = /([^&;=]+)=?([^&;]*)/g,
-       q = window.location.hash.substring(1);
-   e = r.exec(q)
-   while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
-   }
-   return hashParams;
- }
- getUsersPlaylists() {
-   spotifyWebApi.getUserPlaylists()
-     .then((response) => {
-       this.setState({
-         userPlaylists: Array.from(response.items)
-       })
-     }
-     )
+  super();
+  const params = this.getHashParams();
+  spotifyWebApi.setAccessToken(params.access_token);
+  this.state = {
+    userPlaylists : [],
+    playlistOne : {
+      collaborative: false,
+      external_urls: null,
+      href: null,
+      id: '',
+      images:[],
+      name: '',
+      owner: null,
+      public: null,
+      snapshot_id: '',
+      tracks: null,
+      type: '',
+      uri: '',
+   },
+   pName: 'not changed',
+   check1: false,
   }
-
-printButtons() {
-  this.setState({
-    namesList: 
-      <div class = "button-group">
-        {this.state.userPlaylists.map(x => <button>{x.name}</button>)}
-      </div>
-  })
 }
+ getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  e = r.exec(q)
+  while (e) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+     e = r.exec(q);
+  }
+  return hashParams;
+}
+ getUsersPlaylists() {
+  spotifyWebApi.getUserPlaylists()
+    .then((response) => {
+      this.setState({
+        userPlaylists: Array.from(response.items),
+      })
+    }
+    )
+ }
+ onClick1 = (playlist) => {
+   if (this.state.check1 === false) {
+     this.setState({
+       playlistOne : playlist,
+       check1: true,
+       pName: playlist.name
+     }, () => {
+       console.log(this.state.pName);
+     })
+   }
+ }
+
 
  render() {
-   this.getUsersPlaylists()
-   this.printButtons()
-   return <Router>
-    <Route exact path = "/" render = {
-      () => {
-        return ( 
-          <a href= 'http://localhost:8888'>
-          <button>
-            Login with Spotify
-          </button>
-          </a>    
-        )
-      }
-    }/>
-    <Route exact path = "/choice" render = {
-      () => {
-        return ( 
-          <div>
-            <h1>Click on 2 playlists</h1>
-            {this.state.namesList}
-          </div>
-
-        )
-      }
-    }/>
-    <Route exact path = "/result" render = {
-      () => {
-        return ( 
-          <div>
-          <h1>hi</h1>
-        </div>
-        )
-      }
-    }/>
-    </Router>
- }
+  this.getUsersPlaylists()
+  return <Router history={browserHistory}>
+   <Route exact path = "/" render = {
+     () => {
+       return (
+         <a href= 'http://localhost:8888'>
+         <button>
+           Login with Spotify
+         </button>
+         </a>   
+       )
+     }
+   }/>
+   <Route exact path = "/choice" render = {
+     () => {
+       return (
+         <div>
+           {this.state.userPlaylists.map((x, index) =>
+              <a href = 'http://localhost:3000/result/#'>
+             <button key = {index} onClick = { () => this.onClick1(x)}>
+               {x.name}
+             </button>
+             </a>
+           )}
+         </div>
+       )
+     }
+   }/>
+   <Route exact path = "/result" render = {
+     () => {
+       return (
+        <h1>Playlist name: {browserHistory.push({state: {pName: this.state.pName}})}</h1>
+       )
+     }
+   }/>
+   </Router>
+}
 }
 export default App;
+
